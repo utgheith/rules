cdd834868dfef0f334d4fa920deb249d6c320b93
utgheith <utgheith@users.noreply.github.com>
2024-12-10 08:53:48 -0600

    simplified project.scala

--- project.scala
+++ project.scala
@@ -2,20 +2,13 @@
 //> using options -Wall -Wconf:any:e -Wunused:all -feature -deprecation -unchecked -Yexplicit-nulls
 //> using jvm graalvm-java21:21
 
-//> using publish.organization io.github.utgheith
-//> using publish.name rules
-// using publish.version 0.1.0-SNAPSHOT
-//> using publish.computeVersion git:tag
-//> using publish.repository github:io.github.utgheith/rules
+// using publish.organization io.github.utgheith
+// using publish.name rules
+// using publish.computeVersion git:tag
 //> using publish.doc false
 
-//> using dep com.lihaoyi::fansi:0.5.0
-//> using dep com.lihaoyi::mainargs:0.7.6
 //> using dep com.lihaoyi::os-lib:0.11.3
-//> using dep com.lihaoyi::pprint:0.9.0
 //> using dep com.lihaoyi::sourcecode:0.4.2
 //> using dep com.lihaoyi::upickle:4.0.2
 //> using test.dep org.scalameta::munit::1.0.3
-// using dep com.typesafe.scala-logging::scala-logging:3.9.5
-
 
